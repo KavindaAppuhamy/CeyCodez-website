7338da86a3b4073281ca2a4fcc4f7a9eea9e2d73
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-10-30 13:07:17 +0530

    initializing my repository

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+import './App.css'
+import HomePage from "./pages/homePage";
+
+function App() {
+
+  return (
+    <>
+      <Router>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+        </Route>
+      </Routes>
+     </Router>
+    </>
+  )
+}
+
+export default App
